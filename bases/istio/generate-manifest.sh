84fd94ad9fd93696300dd57959167ef2eff3117e
ibraheemalsaady <ibraheem.al-saady@outlook.com>
2020-06-26 15:08:48 +0200

    external dns new encrypted values

--- bases/istio/generate-manifest.sh
+++ bases/istio/generate-manifest.sh
@@ -5,5 +5,8 @@ docker container run \
   --rm \
   -v "$PWD":"$PWD" \
   -w "$PWD" \
-  istio/istioctl:1.6.1 \
+  istio/istioctl:1.6.3 \
     manifest -f custom-config.yaml generate > ./generated-manifest.yaml
+    # profile list
+    # profile dump default > tes.yaml
+    
